f62e0a54b9827cd05a23a587d6ec599f93a114fe
Milos Popovic <milos_popovic@outlook.com>
2020-07-10 22:51:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map { |n| "#{n}!"}
+  array.map { |str| str << '!' }
   #array.map do |element|
   #   "#{element}!"
   end
