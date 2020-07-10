2c8e1c05167322ded4748357c3aa46405c47e8cd
Milos Popovic <milos_popovic@outlook.com>
2020-07-10 23:13:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  #array.any? {|i| i > 4}
+  array.all? {|i| i > 4}
   array.each do |str|
-    return true if str.size > 4
+    
   end
 end
 
