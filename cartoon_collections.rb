7ed18c08e3a11f6bdee45ed0d3b0abb7f2166b83
Milos Popovic <milos_popovic@outlook.com>
2020-07-11 00:06:43 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |str| str << '!' }    #one line map method
+  array.map { |str| str.capitalize }    #one line map method
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(array)
+  array.any? {|i| i.length > 4}   #we can't just leave it at i, we need to use
+end                               # i.length because i in this case is a string
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #array.include?(cheese_types)
+  array.find do |type|
+    cheese_types.include?(type)
+  end
+
 end
